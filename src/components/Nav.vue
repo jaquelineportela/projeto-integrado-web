39c0caad05291df5e37742affd573476e8bd973d
mariacoelh <oliveiracoelhomaria@alu.ufc.br>
2021-08-05 08:57:27 -0300

    Redimensionando os botões da nav.vue

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,20 @@
 <template>
     <nav class="navbar navbar-expand navbar-light fixed-top">
       <div class="container fluid">
-        <a href="#" class="navbar-brand">Home</a>
+        <a href="#" class="navbar-brand">Troca Mágica</a>
         <div class="collapse navbar-collapse">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-                <a href="#" class="nav-link">Entrar</a>
+                <a href="http://localhost:8080/" class="nav-link">Home</a>
             </li>
             <li class="nav-item">
-                <a href="#" class="nav-link">Cadastre-se</a>
+                <a href="#" class="nav-link">Novos</a>
+            </li>
+            <li class="nav-item">
+                <a href="http://localhost:8080/login" class="nav-link">Entrar</a>
+            </li>
+            <li class="nav-item">
+                <a href="http://localhost:8080/cadastrar" class="nav-link">Cadastre-se</a>
             </li>
           </ul>
         </div>
